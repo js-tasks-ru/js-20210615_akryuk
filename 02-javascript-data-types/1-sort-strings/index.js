acbee32e2d34a2d30981aebce9f6fe6ba55cab64
Alexander Kryuk <akryuk@beetsoft.com>
2021-06-22 20:57:16 +0600

    Homework #2 - String compare function update

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,7 +5,7 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-  const compareFn = (prev, next) => prev.localeCompare(next, [], {caseFirst: 'upper'});
+  const compareFn = (prev, next) => prev.localeCompare(next, ['ru', 'en'], {caseFirst: 'upper'});
   const sortedArray = [...arr].sort(compareFn);
   return param !== 'desc' ? sortedArray : sortedArray.reverse();
 }
