4ed9e833c5fde10b970f83fb13f607a7819f9c4a
Alexander Kryuk <akryuk@beetsoft.com>
2021-06-25 18:45:12 +0600

    Homework #3 - Trim strings first solution

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,16 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  const reducer = (acc, current, index, array) => {
+    if (array[index - 1] !== current) {
+      return [...acc, [current]];
+    } else {
+      const accCopy = [...acc];
+      const newLastElem = accCopy.pop() + current;
+      return [...accCopy, newLastElem];
+    }
+  };
 
+  const result = string.split('').reduce(reducer, []);
+  return result.map(item => item.slice(0, size)).join('');
 }
