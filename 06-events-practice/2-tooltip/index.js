770afe7db4b8994c6152a9f68e7fcd0763dff6bb
Alexander Kryuk <akryuk@beetsoft.com>
2021-07-06 11:09:15 +0600

    Homework #6 - Tooltip listeners fixed

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -3,6 +3,12 @@ class Tooltip {
   static instance;
   static offset = 12;
 
+  listeners = {
+    pointerover: this.handlePointerOver.bind(this),
+    pointerout: this.handlePointerOut.bind(this),
+    pointerMove: this.handlePointerMove.bind(this)
+  }
+
   constructor() {
     if (!Tooltip.instance) {
       Tooltip.instance = this;
@@ -21,11 +27,17 @@ class Tooltip {
     this.element.innerHTML = text;
   }
 
-  destroy() {
+  remove() {
     if (this.element) {
       this.element.remove();
       this.element.innerHTML = '';
     }
+  }
+
+  destroy() {
+    this.remove();
+    document.removeEventListener('pointerover', this.listeners.pointerover);
+    document.removeEventListener('pointerout', this.listeners.pointerout);
   }
 
   handlePointerMove(event) {
@@ -33,24 +45,26 @@ class Tooltip {
     this.element.style.top = event.y + Tooltip.offset + 'px';
   }
 
+  handlePointerOver(event) {
+    const tooltipEl = event.target.closest('[data-tooltip]');
+    if (tooltipEl) {
+      this.render(tooltipEl.dataset.tooltip);
+      tooltipEl.addEventListener('pointermove', this.listeners.pointerMove);
+    }
+  }
+
+  handlePointerOut(event) {
+    const tooltipEl = event.target.closest('[data-tooltip]');
+    if (tooltipEl) {
+      this.remove();
+      tooltipEl.removeEventListener('pointermove', this.listeners.pointerMove);
+    }
+  }
+
   initialize() {
     this.createElement();
-
-    document.addEventListener('pointerover', (e) => {
-      const tooltipEl = e.target.closest('[data-tooltip]');
-      if (tooltipEl) {
-        this.render(tooltipEl.dataset.tooltip);
-        tooltipEl.addEventListener('pointermove', e => this.handlePointerMove(e));
-      }
-    });
-
-    document.addEventListener('pointerout', (e) => {
-      const tooltipEl = e.target.closest('[data-tooltip]');
-      if (tooltipEl) {
-        this.destroy();
-        tooltipEl.removeEventListener('pointermove', (e) => this.handlePointerMove(e));
-      }
-    });
+    document.addEventListener('pointerover', this.listeners.pointerover);
+    document.addEventListener('pointerout', this.listeners.pointerout);
   }
 }
 
