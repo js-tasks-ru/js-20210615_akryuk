413e6224e546cfcfadd409e7e518459d07070218
Alexander Kryuk <akryuk@beetsoft.com>
2021-07-12 11:23:54 +0600

    Homework #8 - Product form render static markup and actual categories list

--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -5,11 +5,110 @@ const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ProductForm {
+
+  subElements = {}
+
   constructor (productId) {
     this.productId = productId;
+    this.render();
   }
 
   async render () {
+    this.element = document.createElement('div');
+    this.element.className = 'product-form';
 
+    const productData = await fetchJson(`${BACKEND_URL}/api/rest/products?id=${this.productId}`);
+    const categories = await fetchJson(`${BACKEND_URL}/api/rest/categories?_sort=weight&_refs=subcategory`);
+
+    console.log(productData);
+
+    this.element.innerHTML = `
+      <form data-element="productForm" class="form-grid">
+        <div class="form-group form-group__half_left">
+          <fieldset>
+            <label class="form-label">Название товара</label>
+            <input required="" type="text" name="title" class="form-control" placeholder="Название товара">
+          </fieldset>
+        </div>
+        <div class="form-group form-group__wide">
+          <label class="form-label">Описание</label>
+          <textarea required="" class="form-control" name="description" data-element="productDescription" placeholder="Описание товара"></textarea>
+        </div>
+        <div class="form-group form-group__wide" data-element="sortable-list-container">
+          <label class="form-label">Фото</label>
+          <div data-element="imageListContainer"><ul class="sortable-list"><li class="products-edit__imagelist-item sortable-list__item" style="">
+            <input type="hidden" name="url" value="https://i.imgur.com/MWorX2R.jpg">
+            <input type="hidden" name="source" value="75462242_3746019958756848_838491213769211904_n.jpg">
+            <span>
+          <img src="icon-grab.svg" data-grab-handle="" alt="grab">
+          <img class="sortable-table__cell-img" alt="Image" src="https://i.imgur.com/MWorX2R.jpg">
+          <span>75462242_3746019958756848_838491213769211904_n.jpg</span>
+        </span>
+            <button type="button">
+              <img src="icon-trash.svg" data-delete-handle="" alt="delete">
+            </button></li></ul></div>
+          <button type="button" name="uploadImage" class="button-primary-outline"><span>Загрузить</span></button>
+        </div>
+        <div class="form-group form-group__half_left">
+          <label class="form-label">Категория</label>
+          ${this.renderCategorySelect(categories)}
+        </div>
+        <div class="form-group form-group__half_left form-group__two-col">
+          <fieldset>
+            <label class="form-label">Цена ($)</label>
+            <input required="" type="number" name="price" class="form-control" placeholder="100">
+          </fieldset>
+          <fieldset>
+            <label class="form-label">Скидка ($)</label>
+            <input required="" type="number" name="discount" class="form-control" placeholder="0">
+          </fieldset>
+        </div>
+        <div class="form-group form-group__part-half">
+          <label class="form-label">Количество</label>
+          <input required="" type="number" class="form-control" name="quantity" placeholder="1">
+        </div>
+        <div class="form-group form-group__part-half">
+          <label class="form-label">Статус</label>
+          <select class="form-control" name="status">
+            <option value="1">Активен</option>
+            <option value="0">Неактивен</option>
+          </select>
+        </div>
+        <div class="form-buttons">
+          <button type="submit" name="save" class="button-primary-outline">
+            Сохранить товар
+          </button>
+        </div>
+      </form>
+    `;
+    this.setSubElements();
+  }
+
+  renderCategorySelect(categoriesList = []) {
+
+    // Extracting subcategories from each category to flat array
+    const list = categoriesList.flatMap(
+      category => category.subcategories.map(
+        subcategory => ({id: subcategory.id, title: subcategory.title, rootTitle: category.title})
+      )
+    );
+
+    const options = list.map(
+      item => `<option value="${item.id}">${item.rootTitle} &gt; ${item.title}</option>`
+    ).join('');
+
+    return `
+      <select class="form-control" name="subcategory">
+        ${options}
+      </select>
+    `;
+  }
+
+  setSubElements() {
+    const subs = this.element.querySelectorAll('[data-element]');
+    [...subs].forEach(sub => {
+      const key = sub.dataset.element;
+      this.subElements[key] = sub;
+    });
   }
 }
