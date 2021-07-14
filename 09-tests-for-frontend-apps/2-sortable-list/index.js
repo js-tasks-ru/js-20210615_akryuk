727cb87b2d7a8e9681142c30ca92322e5ad79d80
Alexander Kryuk <akryuk@beetsoft.com>
2021-07-14 14:34:58 +0600

    Homework #9 - Render items and delete

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,43 @@
 export default class SortableList {
+  constructor({items}) {
+    this.items = items;
+    this.render();
+  }
 
+  onDelete = (e) => {
+    const deleteBtn = e.target.closest(`[data-delete-handle]`);
+    if (deleteBtn) {
+      deleteBtn.closest('.sortable-list__item').remove();
+    }
+  }
+
+  render() {
+    this.element = document.createElement('ul');
+    this.element.className = 'sortable-list';
+    this.items.forEach(item => {
+      item.classList.add('sortable-list__item');
+      this.element.append(item);
+    });
+
+    this.initEventListeners();
+  }
+
+  initEventListeners() {
+    document.addEventListener('click', this.onDelete);
+  }
+
+  removeEventListeners() {
+    document.removeEventListener('click', this.onDelete);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.removeEventListeners();
+    this.remove();
+  }
 }
